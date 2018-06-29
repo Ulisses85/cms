71e7db55e5489ad9ae8a8923b84eceb189360550
Ulisses85 <lukasz.kuligowski@gmail.com>
2018-06-29 14:59:25 +0100

    push

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
